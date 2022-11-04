4c44103a0c60be278f8b007adce575bca9746624
sonnysam <samuelagbenyo067@gmail.com>
2022-11-04 08:02:33 +0000

    animation added to the img

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,21 +7,25 @@ const Nav = () => {
   return (
     <nav className="py-3 mb-12 flex justify-between text-white">
       <h1 className="text-xl font-extrabold flex items-center lg:text-3xl md:text-2xl sm:text-2xl">
-        Sonnytech Academy{" "}
-        <span className="text-sky-300 mx-2 text-xl">
+        Sonnytech Academy
+        <span className="text-sky-300 text-xl">
+          {" "}
           <GoVerified />
         </span>
       </h1>
 
-      <ul className="flex items-center sm:w-[10] h-[10]">
+      <ul className="flex items-center">
         <li>
-          <a href="https://github.com/Sonnysam">
+          <a href="https://www.youtube.com/@SonnyTechAcademy">
             <Image
               src={Me}
               alt="Picture of the me"
               width={70}
               height={70}
-              className="rounded-full p-3"
+              sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+              className="rounded-full p-3 animate-pulse"
             />
           </a>
         </li>
