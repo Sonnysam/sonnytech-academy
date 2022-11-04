f69bf977eadec5d4a8bff6b26ad35469b305bb89
sonnysam <samuelagbenyo067@gmail.com>
2022-11-04 09:03:01 +0000

    typewriter complete

--- components/TypeWriter.jsx
+++ components/TypeWriter.jsx
@@ -2,24 +2,26 @@ import { TypeAnimation } from "react-type-animation";
 
 const TypeWriter = () => {
   return (
-    <div className="mt-2 text-gray-300 text-sm flex">
+    <div className="mt-2 text-gray-500 text-base font-bold lg:text-xl flex justify-center items-center">
       <h3 className="mr-1">Learn to be a master of</h3>
       <TypeAnimation
-    // Same String at the start will only be typed once, initially
-    sequence={[
-    'web development',
-    1000,
-    'mobile development',
-    1000,
-    'designs',
-    1000
-    ]}
-    speed={50} 
-    style={{ fontSize: '1em' }}
-    wrapper="span" 
-    repeat={Infinity} 
-    className="text-yellow-300 font-semibold"
-  />
+        // Same String at the start will only be typed once, initially
+        sequence={[
+          "web development",
+          1000,
+          "mobile development",
+          1000,
+          "designs",
+          1000,
+          "wordpress",
+          1000,
+        ]}
+        speed={50}
+        // style={{ fontSize: "1em" }}
+        wrapper="span"
+        repeat={Infinity}
+        className="text-yellow-300 font-extrabold"
+      />
     </div>
   );
 };
