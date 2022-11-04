6e8f5921d5f6ac44afcb7415522b7dd64fffdc6a
sonnysam <samuelagbenyo067@gmail.com>
2022-11-04 16:32:37 +0000

    images added

---
+++ components/Button.jsx
@@ -0,0 +1,11 @@
+
+
+const Button = () => {
+  return (
+    <div className="mt-5 flex justify-center items-center text-white">
+      <a href="https://www.youtube.com/@SonnyTechAcademy" className="bg-yellow-300 px-6 py-4 rounded-full text-base font-bold hover:bg-yellow-400 cursor-pointer">Explore More</a>
+    </div>
+  );
+}
+
+export default Button
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Banner from "../components/Banner";
 import TypeWriter from "../components/TypeWriter";
+import Button from "../components/Button";
 
 
 
 export default function Home() {
   return (
-    <div className="px-5 min-h-screen bg-slate-800">
+    <div className="min-h-screen bg-slate-800">
       <Header />
       <Nav />
       <Banner/>
       <TypeWriter/>
+      <Button/>
     </div>
   );
 }
