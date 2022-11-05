4d6455588fae54e333f8eba0d7117ba43961ec29
sonnysam <samuelagbenyo067@gmail.com>
2022-11-05 07:18:21 +0000

    email complete

--- components/Email.jsx
+++ components/Email.jsx
@@ -1,7 +1,8 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import Button from "./Button";
 
- const Email = () => {
+const Email = () => {
   const form = useRef();
 
   const sendMsg = (e) => {
@@ -25,16 +26,62 @@ import emailjs from "@emailjs/browser";
 
     e.target.reset();
   };
+
   return (
-    <form ref={form} onSubmit={sendMsg}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <article>
+      <h1 className="flex justify-center items-center mt-5 text-white font-bold text-3xl">
+        Let’s keep in touch
+      </h1>
+      <h3 className="flex justify-center items-center mt-2 mb-4 text-white font-bold text-sm">
+        Subscribe to our newsletter & receive the latest updates
+      </h3>
+      <section className=" ">
+        <form
+          ref={form}
+          onSubmit={sendMsg}
+          className=" flex
+        flex-col
+        justify-center
+        items-center
+        w-full p-2"
+        >
+          <label className="text-white font-base flex justify-start">Name</label>
+          <div className="border-2 py-2 px-3 rounded mb-4 bg-[#E2F1F6]">
+            <input
+              type="text"
+              name="user_name"
+              className="outline-none border-none bg-transparent py-2 overflow-hidden w-96"
+            />
+          </div>
+          <label className="text-white font-base flex justify-start">
+            Email
+          </label>
+          <div className="border-2 py-2 px-3 rounded  bg-[#E2F1F6]">
+            <input
+              type="email"
+              name="user_email"
+              className="outline-none border-none bg-transparent py-2 overflow-hidden w-96"
+            />
+          </div>
+          <div className="flex justify-start mt-1 w-96">
+            <input type="checkbox" name="" id="" />
+            <h5 className="text-white font-bold text-sm ml-2">
+              Send me weekly emails only
+            </h5>
+          </div>
+
+          <div className="border-none py-4 px-3 mt-4 rounded mb-4 bg-yellow-300 flex justify-center items-center">
+            <button
+              type="submit"
+              placeholder="Subscribe"
+              className="outline-none border-none font-bold text-white text-xl bg-transparent overflow-hidden w-96"
+            >
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </section>
+    </article>
   );
 };
 
