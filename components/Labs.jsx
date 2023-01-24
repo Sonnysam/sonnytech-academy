26a38213a24fe3cdc1a20fcae5fdbe0679911bc0
sonnysam <samuelagbenyo067@gmail.com>
2023-01-24 10:19:13 -0800

    changes made to labs component

--- components/Labs.jsx
+++ components/Labs.jsx
@@ -2,27 +2,42 @@ import React from "react";
 
 const Labs = () => {
   return (
-    <div className="my-3 flex flex-col justify-center items-center mx-2">
-      <h1 className="mb-2 text-white text-2xl font-bold md:text-2xl sm:xl lg:text-4xl">
-        Introducing{" "}
-        <span className="text-[#FFDE57]">Sonnytech Academy Labs</span>🎉
-      </h1>
-      <p className="flex justify-center items-center text-white text-base sm:text-base md-text-xl lg:text-xl">
-        Sonnytech Academy Labs is a space in Sonnytech
-        Academy dedicated to the development and testing of software and
-        also building open source projects for the community. It provides resources, mentorship, and guidance to entrepreneurs and startups looking to develop their ideas into successful products. The lab offers access to the latest technology, tools, and resources needed to create innovative software solutions. It also provides a collaborative environment for entrepreneurs to network with other like-minded individuals and share ideas.
-      </p>
-    </div>
-    // 1. Ideation and Conceptualization 
-    // 2. Prototyping and Design 
-    // 3. Development and Testing 
-    // 4. Launch and Iteration 
-    // 5. Data Analysis and Insights 
-    // 6. User Experience (UX) Design 
-    // 7. Business Modeling and Strategy 
-    // 8. Marketing and Growth Hacking 
-    // 9. Product Management 
-    // 10. Quality Assurance
+    <>
+      <div className="my-3 flex flex-col justify-center items-center mx-2">
+        <h1 className="mb-2 text-white text-2xl font-bold md:text-2xl sm:xl lg:text-4xl">
+          Introducing{" "}
+          <span className="text-[#FFDE57]">Sonnytech Academy Labs</span>🎉
+        </h1>
+        <p className="text-white text-base sm:text-base md-text-xl lg:text-xl">
+          Sonnytech Academy Labs is a space in Sonnytech Academy dedicated to
+          the development and testing of software and also building open source
+          projects for the community. It provides resources, mentorship,
+          internships and guidance to entrepreneurs and startups looking to
+          develop their ideas into successful products. <br /> The lab offers
+          access to the latest technology, tools, and resources needed to create
+          innovative software solutions. It also provides a collaborative
+          environment for entrepreneurs to network with other like-minded
+          individuals and share ideas.
+        </p>
+      </div>
+      {/* <section className="mx-3 mt-5">
+        <div>
+          <h1 className="text-white text-2xl font-bold mt-3">
+            Some of our focus areas
+          </h1>
+          <p>
+            <ul className="text-[#FFDE57] hover:cursor-pointer text-base sm:text-base md-text-xl lg:text-xl">
+              <li>Tech Girl Child Education</li>
+              <li>Development and Testing</li>
+              <li>Product Management</li>
+              <li>Prototyping and Design</li>
+              <li>Quality Assurance</li>
+              <li>User Experience (UX) Design</li>
+            </ul>
+          </p>
+        </div>
+      </section> */}
+    </>
   );
 };
 
