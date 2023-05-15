2e89b721e1de3bec3885e4cd9b405dcc10fb0506
sonnysam <samuelagbenyo067@gmail.com>
2023-05-15 05:35:34 +0000

    faq data array object added

---
+++ data/data.js
@@ -0,0 +1,27 @@
+const faqs = [
+    {
+      question: 'Are your tutorials suitable for beginners?',
+      answer: 'Yes, we provide tutorials for all skill levels, including beginners. We have introductory tutorials and step-by-step guides to help you get started.'
+    },
+    {
+      question: 'Do I need any prior programming knowledge to follow your tutorials?',
+      answer: 'Some tutorials may assume basic programming knowledge, while others are designed for complete beginners. We recommend checking the difficulty level or prerequisites mentioned in the tutorial description.'
+    },
+    {
+      question: 'How can I support your channel?',
+      answer: 'We appreciate your support! You can support our channel by subscribing to our YouTube channel, liking and sharing our videos, and leaving comments with your feedback. You can also consider joining our Patreon or making a donation through our website.'
+    },
+    {
+      question: 'Can I contribute to your tutorials or website?',
+      answer: "We welcome contributions! If you have a tutorial idea, improvement suggestion, or want to collaborate, please reach out to us through our contact page. We appreciate the community's involvement in making our content better."
+    },
+    {
+      question: 'Can I use your code examples in my own projects?',
+      answer: 'Yes, you are free to use the code examples provided in our tutorials for your own projects. However, we encourage understanding the code and adapting it to your specific needs rather than blindly copying.'
+    },
+    {
+      question: 'How can I get notified of new tutorials?',
+      answer: 'To stay updated with our latest tutorials, you can subscribe to our YouTube channel and turn on notifications. You can also sign up for our newsletter on our website to receive email notifications about new tutorials and other announcements.'
+    },
+  ];
+  
